9e9b9d4339439e94bf0d9af90078874495a8b16c
forsytgrac <66290469+forsytgrac@users.noreply.github.com>
2020-06-14 19:33:34 +1200

    Updated motor movement file
    Instructions on turning left and right or going straight to be used in robot.cpp for completion as well as a completed working program for core that now makes the last turn and runs smoothly through the maze.

---
+++ motorMovement.hpp
@@ -0,0 +1,44 @@
+
+#include "imageProcessing.hpp"
+
+class Motor{
+	private:
+	double defSpeed = 25.0;
+    
+    
+    public:
+    double vLeft;
+    double vRight;
+    Motor(){
+	  setMotors(defSpeed,defSpeed);   
+	}
+	
+ void followLine(int error) {
+	 double speedUp = 0.0;
+	 if(abs(error) < 2){
+		 speedUp = 15;
+	 }
+	int difference = error*0.5;
+	vLeft = defSpeed + speedUp + difference;
+    vRight = defSpeed + speedUp - difference;
+    setMotors(vLeft,vRight);   
+    std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
+    
+}
+ void turnLeft(){
+	 vRight = 15;
+	 vLeft = 0;
+	 setMotors(vLeft,vRight);   
+     std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
+ }
+ void turnRight(){
+	 vRight = 0;
+	 vLeft = 15;
+	 setMotors(vLeft,vRight);   
+     std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
+ } 
+	
+	 
+	 // if multiple white lines - always follow left, if line ends turn around 180 degrees
+      
+};
